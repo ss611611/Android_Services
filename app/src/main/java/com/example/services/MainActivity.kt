b860e75d81395f7df098943f107b711c0b2b03c8
ss611611 <ss611611@gmail.com>
2023-11-08 10:29:21 +0800

    init

---
+++ app/src/main/java/com/example/services/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.services
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.services.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.apply {
+            btnStart.setOnClickListener {
+                startService(Intent(this@MainActivity,MyService::class.java))
+            }
+
+            btnStop.setOnClickListener {
+                stopService(Intent(this@MainActivity,MyService::class.java))
+            }
+        }
+    }
+}
